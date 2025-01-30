18e908481c22df7ca2f78b87fe710fe5553a53a8
Mks30d <manish30d@gmail.com>
2025-01-31 02:06:29 +0530

    Completed top NavBar

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment/screens/center_portion/center_portion.dart';
 import 'package:flutter_assignment/screens/menu.dart';
+import 'package:flutter_assignment/screens/navbar.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -12,7 +13,6 @@ class Dashboard extends StatelessWidget {
         children: [
           // menu
           Container(
-            color: Colors.blue,
             width: 220,
             height: double.infinity,
             child: Menu(),
@@ -20,28 +20,26 @@ class Dashboard extends StatelessWidget {
 
           Expanded(
             child: Container(
-              color: Colors.orange,
+              color: Color(0xffF1F1EF),
               // width: double.infinity,
               height: double.infinity,
               child: Column(
                 children: [
                   Container(
-                    color: Colors.deepPurple,
                     width: double.infinity,
                     height: 50,
-                    child: Text("Menu"),
+                    child: DashboardNavbar(),
                   ),
 
                   Expanded(
                     child: Container(
-                      color: Colors.red,
                       width: double.infinity,
                       // height: double.infinity,
                       child: Row(
                         children: [
                           Expanded(
                             child: Container(
-                              color: Colors.grey,
+                              color: Color(0xffF1F1EF),
                               height: double.infinity,
                               child: CenterPortion()
                             ),
